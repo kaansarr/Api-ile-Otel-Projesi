9a11dde82a457abf4d7c906575372ad3ab57f72f
kaansarr <kaansar33@gmail.com>
2023-11-16 17:05:36 +0300

    Abstract Interfaces

---
+++ MyApiProject/ApiConsume/HotelProject.DataAccessLayer/Abstract/IGenericDal.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HotelProject.DataAccessLayer.Abstract
+{
+	public interface IGenericDal<T> where T : class
+	{
+		void Insert(T entity);
+		void Delete(T entity);
+		void Update(T entity);
+		List<T> GetAll();
+		T GetByID(int id);
+
+
+
+	}
+}
