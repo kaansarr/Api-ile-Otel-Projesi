304efb1e4c2603691f061f8da921c50a6f547cc0
kaansarr <kaansar33@gmail.com>
2024-01-18 18:52:00 +0300

    update

---
+++ MyApiProject/Frontend/HotelProject.WebUI/Dtos/LoginDto/LoginUserDto.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Build.Framework;
+using RequiredAttribute = System.ComponentModel.DataAnnotations.RequiredAttribute;
+
+
+namespace HotelProject.WebUI.Dtos.LoginDto
+{
+	public class LoginUserDto
+	{
+        [Required(ErrorMessage = "Kullanıcı Adını Giriniz")]
+
+		public string Username { get; set; }
+
+
+		[Required(ErrorMessage = "Şifreyi Giriniz")]
+
+		public string Password { get; set; }
+
+	}
+}
--- MyApiProject/Frontend/HotelProject.WebUI/Mapping/AutoMapperConfig.cs
+++ MyApiProject/Frontend/HotelProject.WebUI/Mapping/AutoMapperConfig.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using HotelProject.EntityLayer.Concrete;
+using HotelProject.WebUI.Dtos.LoginDto;
 using HotelProject.WebUI.Dtos.RegisterDto;
 using HotelProject.WebUI.Dtos.ServiceDto;
 
@@ -13,6 +14,7 @@ namespace HotelProject.WebUI.Mapping
             CreateMap<UpdateServiceDto, Service>().ReverseMap();
             CreateMap<CreateServiceDto, Service>().ReverseMap();
             CreateMap<CreateNewUserDto, AppUser>().ReverseMap();
+            CreateMap<LoginUserDto, AppUser>().ReverseMap();
         }
     }
 }
